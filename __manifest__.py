ad305a0252d47d7159d0f3ea90777884682af38d
engheshama <heshamal3in@gmail.com>
2024-06-17 23:25:53 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,8 @@
     'version': '15.0.1.3.0',
     'author': 'Hesham ',
     'license': 'LGPL-3',
+    'price': 49.99,
+    'currency': 'EUR',
     'depends': ['base', 'account'],
     'data': [
         'views/res_partner_view.xml',
