339b918197b38d4bf4dc1629571af2f73653339d
engheshama <heshamal3in@gmail.com>
2024-06-17 23:24:58 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,8 @@
 {
     'name': 'Custom Partner Extension',
     'version': '15.0.1.3.0',
+    'author': 'Hesham ',
+    'license': 'LGPL-3',
     'depends': ['base', 'account'],
     'data': [
         'views/res_partner_view.xml',
